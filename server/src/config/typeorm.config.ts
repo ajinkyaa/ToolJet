6642b0b300ef59b257982f3d820c6f6987d50b5d
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-07-19 02:04:45 +0530

    make configService to load values based on app env

---
+++ server/src/config/typeorm.config.ts
@@ -0,0 +1,28 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { LoggerOptions } from 'typeorm';
+
+export default class TypeOrmConfig {
+  static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      database: configService.get<string>('DB_NAME'),
+      port: +configService.get<number>('DB_PORT') || 5432,
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      host: configService.get<string>('DB_HOST'),
+      synchronize: configService.get<boolean>('DB_SYNCHRONIZE') || false,
+      logging: configService.get<LoggerOptions>('DB_LOGGING') || false,
+      migrationsRun: false,
+      entities: ["dist/**/*.entity{.ts,.js}"],
+      migrations: ["dist/migrations/**/*{.ts,.js}"],
+      cli: { "migrationsDir": "migrations" }
+    };
+  }
+}
+
+export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  inject: [ConfigService]
+};
