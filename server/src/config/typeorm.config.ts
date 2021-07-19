c3493e4070426da0e2129f1c46f31158c7eecb70
Akshay Sasidharan <akshaysasidharan93@gmail.com>
2021-07-19 13:10:22 +0530

    set typeorm logging opts as `all`

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -1,5 +1,8 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
 import { LoggerOptions } from 'typeorm';
 
 export default class TypeOrmConfig {
@@ -13,16 +16,18 @@ export default class TypeOrmConfig {
       host: configService.get<string>('DB_HOST'),
       synchronize: false,
       migrationsRun: false,
-      logging: configService.get<LoggerOptions>('DB_LOGGING') || false,
-      entities: ["dist/**/*.entity{.ts,.js}"],
-      migrations: ["dist/migrations/**/*{.ts,.js}"],
-      cli: { "migrationsDir": "migrations" }
+      logging: configService.get<LoggerOptions>('DB_LOGGING') || 'all',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      migrations: ['dist/migrations/**/*{.ts,.js}'],
+      cli: { migrationsDir: 'migrations' },
     };
   }
 }
 
 export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
-  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
-  inject: [ConfigService]
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  inject: [ConfigService],
 };
